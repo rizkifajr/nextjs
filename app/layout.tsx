a6ff885619ac949b25af417348049ca496f20ab6
rizkifajr <148928323+rizkifajr@users.noreply.github.com>
2026-08-02 06:40:18 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Sidebar from "@/components/layout/Sidebar";
+import Sidebar from "nextjs/components/layout/Sidebar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";

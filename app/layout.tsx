3e7f832ed3877f83e88b17ff441afb2316ed6216
rizkifajr <148928323+rizkifajr@users.noreply.github.com>
2026-08-02 06:39:28 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+  <div className="flex">
+    <Sidebar />
+
+    <main className="flex-1 p-6">
+      {children}
+    </main>
+  </div>
+</body>
     </html>
   );
 }
